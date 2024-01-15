57918f855b1b60bd8f1919ab5b50c0ab3796bd40
ashish <ashishchoksi84@gmail.com>
2024-01-15 19:17:30 +0530

    setup pages & routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import {Routes, Route} from 'react-router-dom';
+import SignInForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
 import './globals.css';
+import { Home } from './_root/pages';
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+        <Routes>
+            {/* public routes */}
+            <Route path='/sign-in' element={<SignInForm />} />
+            <Route path='/sign-up' element={<SignupForm />} />
+
+            {/* private routes */}
+            <Route index element={<Home />} />
+        </Routes>
+    </main>
   )
 }
 
---
+++ src/_root/pages/index.ts
@@ -0,0 +1,3 @@
+// since we can have multiple pages it is good idea to have single file that exports all the pages
+
+export {default as Home} from './Home'
