2e8fddc47d9f215a7b9136fa063906a4c9d17faf
ashish <ashishchoksi84@gmail.com>
2024-01-15 19:47:38 +0530

    setup auth layout

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,23 @@
+import { Navigate, Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
+  // declare var/state here
+  let isAuth: boolean = false;
+
+  // write html code here
   return (
-    <div>AuthLayout</div>
+    <>
+      {
+        isAuth ? 
+        (<Navigate to={"/"} />) : 
+        (
+          <section>
+            {/* Outlet will honor child component's path */}
+            <Outlet />
+          </section>
+        )
+      }
+    </>
   )
 }
 
