2a5f524ced6a691a0e8aed0bba8034223cb3e24e
ashish <ashishchoksi84@gmail.com>
2024-01-15 19:38:17 +0530

    setup shadcn lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import {Routes, Route} from 'react-router-dom';
 import SignInForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
 import './globals.css';
 import { Home } from './_root/pages';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
     <main>
         <Routes>
             {/* public routes */}
-            <Route path='/sign-in' element={<SignInForm />} />
-            <Route path='/sign-up' element={<SignupForm />} />
+            <Route element={<AuthLayout />}>
+                <Route path='/sign-in' element={<SignInForm />} />
+                <Route path='/sign-up' element={<SignupForm />} />
+            </Route>
 
             {/* private routes */}
-            <Route index element={<Home />} />
+            <Route element={<RootLayout/>}>
+                <Route index element={<Home />} />
+            </Route>
         </Routes>
     </main>
   )
